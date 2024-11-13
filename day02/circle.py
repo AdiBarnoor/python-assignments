5061ac3735739821664da801aaee2e64e94f4410
AdiBarnoor <adibbb333@gmail.com>
2024-11-13 15:02:19 +0200

    Add files via upload

---
+++ day02/circle.py
@@ -0,0 +1,7 @@
+import math
+radius = input("Radius = ")
+area = math.pi* int(radius) * int(radius)
+circumference = math.pi * 2 * int(radius)
+
+print(f"Area of the circle: {area}")
+print(f"Circumference of the circle: {circumference}")
